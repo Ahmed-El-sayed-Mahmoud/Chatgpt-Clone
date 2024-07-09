9c09c6f5359892f6f573f5d9ad481246d8c897d0
Ahmed-El-sayed-Mahmoud <130814088+Ahmed-El-sayed-Mahmoud@users.noreply.github.com>
2024-07-09 05:34:00 +0300

    Update index.js

--- chatgpt-backend/index.js
+++ chatgpt-backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors({
     credentials:true,
-    origin: ["https://chatgpt-clone-frontend-kappa.vercel.app/"]
+    origin: ["https://chatgpt-clone-frontend-kappa.vercel.app"]
 }))
 app.use(CookieParser())
 app.use(morgan('dev'));
